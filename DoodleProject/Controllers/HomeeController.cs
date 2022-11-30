6896ab3986f7c0e3eea5a15563302618be06186c
Ensar CAN <ensarcan57@gmail.com>
2022-11-30 10:44:26 +0300

    Add project files.

---
+++ DoodleProject/Controllers/HomeeController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Parser.SyntaxTree;
+using System.Web.UI.WebControls;
+
+namespace DoodleProject.Controllers
+{
+    public class HomeeController : Controller
+    {
+
+        Mail email;
+
+        public HomeeController()
+        {
+            email = new Mail();
+        }
+        [HttpGet]
+        public ActionResult EMailSender()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EMailSender(string sendMailAdress, string subject, string body)
+        {
+            email.mail(sendMailAdress, subject, body);
+            ViewBag.Kontrol = "Başarı İle Mail Gönderildi";
+            return View();
+        }
+
+
+    }
+}
---
+++ DoodleProject/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using DoodleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoodleProject.Controllers
+{
+    public class MailController : Controller
+    {
+        //[HttpGet]
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Index(MailRequest mailRequest)
+        //{
+        //    MimeMessage mimeMessage = new MimeMessage();
+           
+        //    MailAddress mailAddressFrom = new MailAddress(mailRequest.Name, mailRequest.SenderMail);
+        //    mimeMessage.From.Add(mailAddressFrom);
+        //    MailAddress mailAddressTo = new MailAddress("User", mailRequest.ReceiverMail);
+        //    mimeMessage.To.Add(mailAddressTo);
+        //    mimeMessage.Body=mailRequest.Body;
+        //    mimeMessage.Subject=mailRequest.Subject;
+        //    mimeMessage.Body=mailRequest.Body;
+        //    SmtpClient client= new SmtpClient();
+        //    client.Connect("smtp.gmail.com", 587, false);
+        //    //client.Authenticate()
+        //    client.Disconnect(true);
+        //    return View();
+        //}
+
+
+    }
+}
